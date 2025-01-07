43d9ba816e45ed3c57d8a29548de7f13402a580e
Jonathon Tech <jgt.tech@protonmail.com>
2025-01-06 21:51:21 -0600

    WIP (1736221881)

--- tools/go/cmds/install/alacritty/Install.go
+++ tools/go/cmds/install/alacritty/Install.go
@@ -1,37 +1,42 @@
 package alacritty
 
 import (
-	// "errors"
-	// "fmt"
-	// "jgttech/dotfiles/src/assert"
+	"errors"
+	"fmt"
+	"jgttech/dotfiles/src/assert"
 	"jgttech/dotfiles/src/context"
-	// "jgttech/dotfiles/src/exec"
-	// "jgttech/dotfiles/src/os"
-	// _os "os"
-	// "path"
-	// "runtime"
+
+	"jgttech/dotfiles/src/exec"
+	"jgttech/dotfiles/src/os"
+	_os "os"
+	"path"
+	"runtime"
 )
 
-// const (
-// 	dir  = "packages/alacritty/.config/alacritty"
-// 	file = "alacritty.toml"
-// )
+const (
+	dir        = "packages/alacritty/.config/alacritty"
+	configFile = "alacritty.toml"
+)
 
 func Install(etx *context.ExecutionContext) {
-	// cfg := etx.Build.Config
-	// name := "alacritty." + runtime.GOOS + ".toml"
-	// base := path.Join(cfg.Base, alacrittyDir)
-	// config := path.Join(base, alacrittyConfig)
-	// target := path.Join(base, name)
+	cfg := etx.Build.Config
+	name := "alacritty." + runtime.GOOS + ".toml"
+	base := path.Join(cfg.Base, dir)
+	config := path.Join(base, configFile)
+	target := path.Join(base, name)
 
-	// if os.Exists(config) {
-	// 	_os.Remove(config)
-	// }
+	if os.Exists(config) {
+		_os.Remove(config)
+	}
 
-	// if !os.Exists(target) {
-	// 	panic(errors.New(fmt.Sprintf("Alacritty config not found: '%s'", target)))
-	// }
+	if !os.Exists(target) {
+		panic(errors.New(fmt.Sprintf("Alacritty config not found: '%s'", target)))
+	}
 
-	// cmd := exec.Cmd(fmt.Sprintf("cp -v %s %s", target, cfg), exec.Stdio)
-	// assert.Will(cmd.Run())
+	assert.Will(
+		exec.Cmd(
+			fmt.Sprintf("cp -v %s %s", target, config),
+			exec.Stdio,
+		).Run(),
+	)
 }
